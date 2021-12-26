02ac623919c9bda8bc6008863f94114a186c2873
FlyAbove <76478580+FlyAbove@users.noreply.github.com>
2021-12-27 02:31:47 +0500

    Add files via upload
    Added a easy kick command

---
+++ commands/kick.js
@@ -0,0 +1,44 @@
+const green = '<:tick:818403293977378846>'
+const red = '<:wrong:818403353808732192>'
+
+module.exports = {
+    name: 'kick',
+    description: 'A kick command for moderators',
+    async execute(message, args, client, bot){
+ 
+        if(message.mentions.users.has('919342338902482944'))
+        {message.channel.send({embed:{ title: `${red} Sorry but i dont have the courage to kick myself`, color: 'RED'}})}
+
+       
+        const member = message.mentions.members.first()
+        if(!message.member.hasPermission('KICK_MEMBERS'))
+        return
+        if(!message.guild.me.hasPermission('KICK_MEMBERS')){
+            return message.channel.send({embed:{title: `${red} I don't have __KICK MEMBERS__ permission to execute this command`, color: 'RED'}})
+        }
+
+
+
+        if ( member && member.roles.highest.position > message.guild.me.roles.highest.position || member && member.roles.highest.position == message.guild.me.roles.highest.position || member && member.user.id === message.guild.ownerID) return message.channel.send({embed:{title:` ${red} The mentioned user has a higher role in hierarchy`, color: 'RED'}})
+        if(message.member.hasPermission('ADMINISTRATOR') || message.member.hasPermission('KICK_MEMBERS'))
+        
+        {
+           
+            
+            if(member){
+            message.channel.send({embed:{title:` ${green} ${member.user.tag} is successfully been kicked`, color: message.guild.me.displayColor,}})
+            member.kick()
+            
+            }
+            
+            else{
+                message.channel.send({embed:{ title: `${red} Please mention a valid user you desire to kick from the server`, color: 'RED'}})
+
+            }
+          
+        }
+        
+
+            
+    } 
+}
