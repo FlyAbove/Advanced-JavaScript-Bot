ff1f8ecf9473b01cd5c25aa436a68df58a994ece
FlyAbove <76478580+FlyAbove@users.noreply.github.com>
2021-12-27 17:15:44 +0500

    Delete ping.js

--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-    module.exports = {
-        name: 'ping',
-        description: "A command to tell bot's latency",
-        async execute(message, args, client) { 
-            message.channel.send(`API Latency is ${Math.round(client.ws.ping)}ms.`)
-        }
-    }
